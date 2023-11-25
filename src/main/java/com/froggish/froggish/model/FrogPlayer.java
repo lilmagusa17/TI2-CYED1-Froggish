f72f91e38ebd02da7477f239b36604284388d190
Mariana Agudelo Salazar <magusa2004@gmail.com>
2023-11-25 10:01:46 -0500

    Frog position in graph ok

--- src/main/java/com/froggish/froggish/model/FrogPlayer.java
+++ src/main/java/com/froggish/froggish/model/FrogPlayer.java
@@ -1,9 +1,12 @@
 package com.froggish.froggish.model;
 
+import com.froggish.froggish.graph.GraphAdjacencyList;
+import com.froggish.froggish.graph.Node;
 import com.froggish.froggish.graph.Position;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 import java.util.ArrayList;
@@ -13,10 +16,8 @@ import java.util.TimerTask;
 public class FrogPlayer{
 
     private final String PATH_IDLE = "/com/froggish/froggish/img/frog/idle/i";
-    private final String PATH_JUMPD = "";
-    private final String PATH_JUMPU = "";
     private final String PATH_JUMPR = "/com/froggish/froggish/img/frog/jump/jr";
-    private final String PATH_JUMPL = "";
+    private final String PATH_CROAK = "/com/froggish/froggish/img/frog/croak/c";
     private final String PATH_DEAD = "/com/froggish/froggish/img/frog/death/d";
     private int size;
 
@@ -25,10 +26,8 @@ public class FrogPlayer{
 
     private ArrayList<Image> idles; //estatico
 
-    private ArrayList<Image> jumpsDown; //saltando
-    private ArrayList<Image> jumpsUp; //saltando
     private ArrayList<Image> jumpsRight; //saltando
-    private ArrayList<Image> jumpsLeft; //saltando
+    private ArrayList<Image> croaks; //croando
     private ArrayList<Image> dead; //muerto
 
 
@@ -46,9 +45,11 @@ public class FrogPlayer{
 
     private boolean downPressed;
     private WaterLily waterLily;
-    private ArrayList<Life> energy;
+    private int energy;
 
-    public FrogPlayer(Canvas canvas) {
+    private GraphAdjacencyList<Position> graph;
+
+    public FrogPlayer(Canvas canvas, Position startPosition) {
 
         this.canvas = canvas;
         this.graphicsContext = this.canvas.getGraphicsContext2D();
@@ -58,14 +59,15 @@ public class FrogPlayer{
         this.frame = 0; //cada vez que se corre, hay que cambiar el frame
 
         this.idles = new ArrayList<>();
-        /*this.jumpsDown = new ArrayList<>();
-        this.jumpsUp = new ArrayList<>();*/
+
         this.jumpsRight = new ArrayList<>();
-        //this.jumpsLeft = new ArrayList<>();
+
         this.dead = new ArrayList<>();
 
-        this.position = new Position(70, 240);
-        //0,0-> esquina superior izquierda
+        this.croaks = new ArrayList<>();
+
+        this.position = startPosition;
+
 
         for (int i = 1; i <= 8; i++) { //idle 8
             Image image = new Image(getClass().getResourceAsStream(PATH_IDLE + i + ".png"), 30, 30, false, false);
@@ -81,10 +83,20 @@ public class FrogPlayer{
             Image image = new Image(getClass().getResourceAsStream(PATH_DEAD + i + ".png"), 30, 30, false, false);
             this.dead.add(image);
         }
+
+        for (int i = 1; i <= 8; i++) { //   croak 8
+            Image image = new Image(getClass().getResourceAsStream(PATH_CROAK + i + ".png"), 30, 30, false, false);
+            this.dead.add(image);
+        }
+
+        this.energy = 100;
+
+        this.size = 30;
+
     }
 
     public void stop() {
-        int canvasWidth = 900;  // Set the width of your canvas
+        int canvasWidth = 1000;  // Set the width of your canvas
         int canvasHeight = 600; // Set the height of your canvas
 
         // Calculate limits for the frog player
@@ -101,31 +113,40 @@ public class FrogPlayer{
             this.position.setY(canvasHeight - 30);
             this.downPressed = false;
         }
+
+        if(isHome()){
+            this.state = State.CROAK;
+            this.energy = 100;
+        }
+
     }
 
 
     public void paint() {
         onMove();
         stop();
-//        System.out.println(this.position.getX() + " " + this.position.getY());
+        System.out.println(this.position.getX() + " " + this.position.getY());
+        if(checkWaterLilyCollision(waterLily)){
+            System.out.println("colision");
+            this.state = State.CROAK;
+            //this.energy = 100;
+        }
+
+        System.out.println("no colision");
         switch (state) {
             case IDLE: // idle 8
                 this.graphicsContext.drawImage(idles.get(frame % 8), this.position.getX(), this.position.getY());
                 break;
-            /*case JUMPD: // jump down 2
-                this.graphicsContext.drawImage(jumpsDown.get(frame % 2), this.position.getX(), this.position.getY());
-                break;
-            case JUMPL: // jump left 2
-                this.graphicsContext.drawImage(jumpsLeft.get(frame % 2), this.position.getX(), this.position.getY());
-                break;*/
             case JUMPR: // jump right 7
                 this.graphicsContext.drawImage(jumpsRight.get(frame % 7), this.position.getX(), this.position.getY());
                 break;
-           /* case JUMPU: // jump up 3
-                this.graphicsContext.drawImage(jumpsUp.get(frame % 2), this.position.getX(), this.position.getY());
-                break;*/
+
             case DEAD: // dead son 8
                 this.graphicsContext.drawImage(dead.get(frame % 8), this.position.getX(), this.position.getY());
+                break;
+
+            case CROAK: // croak son 8
+                this.graphicsContext.drawImage(croaks.get(frame % 8), this.position.getX(), this.position.getY());
                 break;
         }
 
@@ -184,7 +205,7 @@ public class FrogPlayer{
         }
     }
 
-    public void onMove() {
+/*   public void onMove() {
         int step = 5;
 
         if (rightPressed) {
@@ -198,15 +219,131 @@ public class FrogPlayer{
         } else if (downPressed && !upPressed) {
             this.position.setY(this.position.getY() + step);
         }
+    }*/
 
+    public void onMove() {
+        int step = 5;
+
+        if (rightPressed && canMoveRight()) {
+            this.position.setX(this.position.getX() + step);
+        } else if (leftPressed && canMoveLeft()) {
+            this.position.setX(this.position.getX() - step);
+        }
+
+        if (upPressed && !downPressed && canMoveUp()) {
+            this.position.setY(this.position.getY() - step);
+        } else if (downPressed && !upPressed && canMoveDown()) {
+            this.position.setY(this.position.getY() + step);
+        }
     }
 
+    private boolean canMoveRight() {
+        // Get the current node
+        Node<Position> currentNode = getCurrentNode();
+
+        // Check if there is a node to the right
+        Node<Position> rightNode = getAdjacentNode(currentNode, Direction.RIGHT);
+        return rightNode != null;
+    }
+
+    private boolean canMoveLeft() {
+        // Get the current node
+        Node<Position> currentNode = getCurrentNode();
+
+        // Check if there is a node to the left
+        Node<Position> leftNode = getAdjacentNode(currentNode, Direction.LEFT);
+        return leftNode != null;
+    }
+
+    private boolean canMoveUp() {
+        // Get the current node
+        Node<Position> currentNode = getCurrentNode();
+
+        // Check if there is a node above
+        Node<Position> upNode = getAdjacentNode(currentNode, Direction.UP);
+        return upNode != null;
+    }
+
+    private boolean canMoveDown() {
+        // Get the current node
+        Node<Position> currentNode = getCurrentNode();
+
+        // Check if there is a node below
+        Node<Position> downNode = getAdjacentNode(currentNode, Direction.DOWN);
+        return downNode != null;
+    }
+
+    private Node<Position> getCurrentNode() {
+        // Get the current position of the frog player
+        Position currentPosition = this.position;
+
+        // Find the corresponding node in the graph
+        return findNodeByPosition(currentPosition);
+    }
+
+    private Node<Position> getAdjacentNode(Node<Position> node, Direction direction) {
+        // Get the list of adjacent nodes
+        ArrayList<Node<Position>> adjacentNodes = node.getNodosAdy();
+
+        // Loop through the adjacent nodes and find the one in the specified direction
+        for (Node<Position> adjacentNode : adjacentNodes) {
+            Position adjacentPosition = adjacentNode.getValor();
+            switch (direction) {
+                case RIGHT:
+                    if (adjacentPosition.getX() > node.getValor().getX()) {
+                        return adjacentNode;
+                    }
+                    break;
+                case LEFT:
+                    if (adjacentPosition.getX() < node.getValor().getX()) {
+                        return adjacentNode;
+                    }
+                    break;
+                case UP:
+                    if (adjacentPosition.getY() < node.getValor().getY()) {
+                        return adjacentNode;
+                    }
+                    break;
+                case DOWN:
+                    if (adjacentPosition.getY() > node.getValor().getY()) {
+                        return adjacentNode;
+                    }
+                    break;
+            }
+        }
+        return null; // No adjacent node in the specified direction
+    }
+
+    private Node<Position> findNodeByPosition(Position position) {
+        // Get the list of nodes in the graph
+
+        ArrayList<Node<Position>> nodes = graph.getNodes();
+
+        // Loop through the nodes and find the one with the specified position
+        for (Node<Position> node : nodes) {
+            if (node.getValor().equals(position)) {
+                return node;
+            }
+        }
+        return null; // Node not found
+    }
+
+
+
     private boolean intersects(WaterLily wl) {
-        // Check if the bounding boxes of this and brick intersect
+        if (wl == null) {
+            return false;  // No collision if waterLily is null
+        }
+
+        // Check if the bounding boxes of this and waterLily intersect
         return this.position.getX() < wl.getPosition().getX() + wl.getSize() &&
                 this.position.getX() + this.getSize() > wl.getPosition().getX() &&
                 this.position.getY() < wl.getPosition().getY() + wl.getSize() &&
                 this.position.getY() + this.getSize() > wl.getPosition().getY();
+    }
+
+    public boolean checkWaterLilyCollision(WaterLily waterLily) {
+        return intersects(waterLily);
     }
 
 
@@ -222,35 +359,38 @@ public class FrogPlayer{
      * Decreases the lives of the player (use when player is killed)
      */
     public void flop(){
-        if(this.energy.size()==1){
+        if(energy == 0){
             this.state=State.DEAD;
 
 
         }else{
-            //FIXME: The frog has to stay in the same position when it loses energy
-            position.setX(20);
-            position.setY(250);
+
+            position.setX(this.position.getX());
+            position.setY(this.position.getY());
             Timer timer=new Timer();
             timer.schedule(new TimerTask() {
                 @Override
                 public void run() {
-                    //FIXME how many lives should we have? Like 100?
-                    energy.remove(2);
+
+                    energy--;
                 }
             }, 800);
 
         }
 
-        System.out.println("lives: " + this.energy.size());
+        System.out.println("lives: " + this.energy);
     }
 
     public boolean isHome(){
-        //FIXME: The coordinates should match the home coordinates -> the las node of the graph
-        return this.position.getX()>=290 && this.position.getX()<=470 && this.position.getY()>=480;
+        return this.position.getX() >= 880 && this.position.getY() == 70;
     }
 
     public int getSize(){
         return this.size;
+    }
+
+    public void setWaterLily(WaterLily waterLily) {
+        this.waterLily = waterLily;
     }
 
 
--- src/main/java/com/froggish/froggish/model/State.java
+++ src/main/java/com/froggish/froggish/model/State.java
@@ -2,5 +2,5 @@ package com.froggish.froggish.model;
 
 public enum State {
 
-    IDLE, JUMPU, JUMPD, JUMPR, JUMPL, DEAD
+    IDLE, JUMPU, JUMPD, JUMPR, JUMPL, DEAD, CROAK
 }
